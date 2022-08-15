44de4fbcc1b8ff1853aca4aa6ea14983f5bc3dc5
Raymond Z <raykyri@gmail.com>
2022-08-15 18:12:26 -0400

    colors and logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
         title: "Canvas Docs",
         logo: {
           alt: "Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
         items: [
           {
